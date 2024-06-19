cbab406456df3cf4fd4eb1273f2499b83778f4e7
Matías Leandro López <diel.job@gmail.com>
2024-06-18 22:47:54 -0300

    borrando papelera

--- papelera/utils/dicts_to_bin.py
+++
@@ -1,9 +0,0 @@
-def dict_to_bin(dic):
-    dic = {bin(k)[2:]: v for k, v in dic.items()}
-    #print(dic)
-    return dic
-
-def dict_to_int(dic):
-    dic = {int(k): v for k, v in dic.items()}
-    #print(dic)
-    return dic
--- papelera/utils/imprimir_log.py
+++
@@ -1,5 +0,0 @@
-def imprimir_log_txt(data, nombre_archivo):
-    # Abre el archivo en modo de escritura
-    with open(nombre_archivo, "a") as file:
-        # Escribe el texto en el archivo
-        print(str(data), file=file)
